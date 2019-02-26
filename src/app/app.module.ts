69ada08729fe0483cc6443b7ca6ed88a82836bd4
Gabriela Flores <gabiiflores09@gmail.com>
2019-02-26 00:09:43 -0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { routing } from './routes.module';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -27,7 +28,8 @@ import { MessageService } from './messages/message.service';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    routing
+    routing,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [
     HeroService,
--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Hero } from './hero';
-import { Heroes} from '../mock-heroes';
 import { Observable, of} from 'rxjs';
 import { MessageService } from '../messages/message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
+    private heroesUrl = '/api/heroes';
 
-    this.messageService.add('HeroService: fetched heroes');
-    return of(Heroes);
+    getHeroes(): Observable<Hero[]> {
 
-  }
-  getHero( id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(Heroes.find(hero => hero.id === id));
+      return this.http.get<Hero[]>(this.heroesUrl);
+    }
+
+    getHero( id: number): Observable<Hero> {
+      this.messageService.add(`HeroService: fetched hero id=${id}`);
+      return of(Heroes.find(hero => hero.id === id));
+    }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
